3d4f5aa313c378242eb44adcc738724db1f1d745
mariiarenatska <mariagernich@gmail.com>
2025-07-28 18:21:27 +0300

    add style

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 2000,
+    autoplaySpeed: 1000,
+    loop: true,
+    nav: true,
+    navText : ["",""],
+    responsive:{
+        0:{
+            items:1
+        },
+        1100:{
+            items:3
+        }
+    }
+
+});
