80564e8db34763fbce16142ca991a481ca2fb34a
mariiarenatska <mariagernich@gmail.com>
2025-07-30 01:00:02 +0300

    add style

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
-$('.owl-carousel').owlCarousel({
-    autoplay:true,
-    autoplayTimeout: 2000,
-    autoplaySpeed: 1000,
-    loop: true,
-    nav: true,
-    navText : ["",""],
-    responsive:{
-        0:{
-            items:1
-        },
-        1100:{
-            items:3
+$(window).on("load", function(){
+
+    /* scroll */
+
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
+
+
+
+    $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        autoplayTimeout: 4000,
+        autoplaySpeed: 2000,
+        loop: true,
+        nav: true,
+        dots: true,
+        margin: 4,
+        center: true,
+        navText : ["",""],
+        responsive:{
+            0:{
+                items:1
+            },
+            1100:{
+                items:3
+            }
         }
-    }
+
+    });
 
 });
+
